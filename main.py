8387e1290de8a4c1c56543fb219a8850bc4d9558
dracangelo <kelvinmbuguaw@gmail.com>
2023-09-20 10:23:40 +0300

    added scraping logic

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ try:
     source.raise_for_status()#raise a status error if any is found
     
     soup = BeautifulSoup(source.txt, 'html.parser')#source.txt returns the html content of the webpage
-    print(soup)
+    
+    movies = soup.find('tbody', class_='lister-list').find_all('tr')
+    
+    for movie in movies:
+        name = movie.find('td', class_='titleColumn').a.text
+        
+        rank = movie.find('td', class_='titleColumn').get_text(strip=True).split('.')[0]
+        
+        year = movie.find('td', class_='titleColumn').span.text.strip('{}')
+        
+        rating = movie.find('td', class_='ratingColumn imdbRating').strong.text
+        
+        print(rank,name,rating,year)
+        
+    
 except Exception as e:
     print (e)
